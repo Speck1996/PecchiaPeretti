27c64e68285500e66196fdc632407a597e905bbe
Edoardo Peretti <edoardo1.peretti@mail.polimi.it>
2018-12-17 19:30:39 +0100

    db schema

---
+++ IT/create.sql
@@ -0,0 +1,92 @@
+DROP DATABASE IF EXISTS trackme;
+
+CREATE DATABASE trackme;
+use trackme;
+
+CREATE TABLE Individual 
+(
+	taxcode VARCHAR (30) PRIMARY KEY,
+	username VARCHAR (40) UNIQUE NOT NULL,
+	name VARCHAR (40),
+	surname VARCHAR (40),
+	email VARCHAR (320) UNIQUE NOT NULL,
+	birth_date DATE,
+	password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE ThirdParty
+(
+	username VARCHAR (30) PRIMARY KEY,
+	email VARCHAR (320) UNIQUE NOT NULL,
+	name VARCHAR (40),
+	surname VARCHAR (40),
+	password VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE Monitoring
+(
+	individual VARCHAR (30),
+	third_party VARCHAR (30),
+	views SMALLINT NOT NULL,
+
+	PRIMARY KEY (individual, third_party),
+	FOREIGN KEY (individual) REFERENCES Individual(taxcode) ON UPDATE CASCADE ON DELETE CASCADE, 
+	FOREIGN KEY (third_party) REFERENCES ThirdParty(username) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE GroupMonitoring
+(
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	third_party VARCHAR (30) NOT NULL,
+	views SMALLINT NOT NULL,
+	location VARCHAR (100),
+	age_min TINYINT,
+	age_max TINYINT,
+	sex ENUM('male', 'female'),
+
+	FOREIGN KEY (third_party) REFERENCES ThirdParty(username) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Heartbeat
+(
+	individual VARCHAR (30),
+	ts TIMESTAMP,
+	value SMALLINT NOT NULL,
+	location VARCHAR (100),
+
+	PRIMARY KEY (individual, ts),
+	FOREIGN KEY (individual) REFERENCES Individual(taxcode) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE BloodPressure
+(
+	individual VARCHAR (30),
+	ts TIMESTAMP,
+	value SMALLINT NOT NULL,
+	location VARCHAR (100),
+
+	PRIMARY KEY (individual, ts),
+	FOREIGN KEY (individual) REFERENCES Individual(taxcode) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE SleepTime
+(
+	individual VARCHAR (30),
+	day date,
+	value time NOT NULL,
+	location VARCHAR (100),
+
+	PRIMARY KEY (individual, day),
+	FOREIGN KEY (individual) REFERENCES Individual(taxcode) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE Steps
+(
+	individual VARCHAR (30),
+	day date,
+	value int NOT NULL,
+	location VARCHAR (100),
+
+	PRIMARY KEY (individual, day),
+	FOREIGN KEY (individual) REFERENCES Individual(taxcode)ON UPDATE CASCADE ON DELETE CASCADE
+);
---
+++ IT/sample.sql
@@ -0,0 +1,29 @@
+use trackme;
+DELETE from Individual;
+DELETE from ThirdParty;
+DELETE from Monitoring;
+DELETE from GroupMonitoring;
+DELETE from HeartBeat;
+DELETE from SleepTime;
+
+INSERT INTO Individual (taxcode, username, name, surname, email, birth_date, password)
+	VALUES	('ABCDE', 'spyro', 'Mario', 'Rossi', 'mario.rossi@mail.com', '1990-10-20', 'aaish'),
+			('FGHI', 'turing', 'John', 'Doe', 'jdoe@lol.com', '1955-11-02', 'qjqn'),
+			('AHJ28', 'boole', NULL , 'Smith', 'smit@me.org', NULL, 'isksi');
+
+INSERT INTO ThirdParty (username, email, name, surname, password)
+	VALUES	('mydoc', 'doc@mail.com', NULL, NULL, 'aanjanjn'),
+			('cardiologist', 'heart@mail.org', 'Luigi', 'Verdi', 'quiqhi');
+
+INSERT INTO Monitoring (individual, third_party, views)
+	VALUES	('ABCDE', 'mydoc', 5);
+
+INSERT INTO GroupMonitoring (third_party, views, location, age_min, age_max, sex)
+	VALUES ('cardiologist', 3, 'Milano', '18', '35', 'female');
+
+INSERT INTO HeartBeat (individual, ts, value, location)
+	VALUES 	('ABCDE', '2018-12-01 16:39:00', 150, '45.4640976,9.191926500000022'),
+			('FGHI', '2018-12-04 11:22:00', 120, '45.4640976,9.191926500000022');
+
+INSERT INTO SleepTime (individual, day, value, location)
+	VALUES	('ABCDE', '2018-12-01 16:39:00', '8:30:00', '45.4640976,9.191926500000022');
